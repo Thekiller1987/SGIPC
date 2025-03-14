64579b66ec224aa9732216cb2d6744d69e1e0814
waskar <waskareliasrios11@gmail.com>
2025-03-14 09:50:37 -0600

    solucion de errores de despliegue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./database/authcontext";
 import ProtectedRoute from "./components/ProtectedRoute"; 
-import Login from './Views/Login';
-import Encabezado from "./Components/Encabezado";
-import Inicio from "./Views/Inicio";
+import Login from './views/Login';
+import Encabezado from "./components/Encabezado";
+import Inicio from "./views/Inicio";
 
 import "./App.css";
 
@@ -17,10 +17,8 @@ function App() {
             <Encabezado />
             <main>
               <Routes>
-                
                 <Route path="/" element={<Login />} />
                 <Route path="/inicio" element={<ProtectedRoute element={<Inicio />} />} />
-
               </Routes>
             </main>
           </div>
@@ -30,4 +28,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
