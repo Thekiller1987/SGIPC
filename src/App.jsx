e93af379b911a607b56a9af2d92ad13179125e2c
Alejandra Baltodano <alejandrabaltodano67@gmil.com>
2025-04-05 21:25:41 -0600

    diseño 2/3 de proveedor xd

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const AppContent = () => {
   const location = useLocation();
 
   // Define las rutas donde NO quieres el header
-  const noHeaderRoutes = ["/gastos-overview", "/proveedores","/presupuesto"];
+  const noHeaderRoutes = ["/gastos-overview", "/proveedores","/presupuesto","/detalle-proveedor"];
 
   return (
     <>
