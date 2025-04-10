082919c3d6c55d95a055c92528d185b48275708c
Daniela Baltodano <alejandrabaltodano67@gmail.com>
2025-04-10 09:06:01 -0600

    cosa q poca gente sabe

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import "../InicioCss/Inicio.css";
+import "../Iniciocss/Inicio.css";
 import Lottie from "lottie-react";
 import GruaAnimacion from "../assets/iconos/animaciongrua.json";
 
