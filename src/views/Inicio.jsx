3b188ed77e882963ad8b2a4f9c22f4c33398fc76
Yamil <Yamilg620@gmail.com>
2025-04-09 21:31:02 -0600

    sidebar

--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -13,14 +13,6 @@ const Inicio = () => {
 
   return (
     <div className="inicio-container">
-<<<<<<< HEAD
-      <div className="inicio-card">
-        <h1 className="inicio-titulo">Bienvenido a ObraTitan</h1>
-        <p className="inicio-texto">
-          Gestiona tus proyectos de construcción de forma integral. Accede rápidamente a tus proyectos, revisa el progreso y administra documentos de manera sencilla.
-        </p>
-        <button className="inicio-boton" onClick={handleGoToProjects}>
-=======
       <div className="inicio-contenido-vertical">
         <div className="inicio-animacion">
           <Lottie animationData={GruaAnimacion} loop={true} />
@@ -32,10 +24,7 @@ const Inicio = () => {
           Gestiona tus proyectos de construcción de forma integral.<br />
           Accede rápidamente a tus proyectos, revisa el progreso y administra documentos.
        </p>
-
-
         <button className="btn-principal" onClick={handleGoToProjects}>
->>>>>>> 6b5a79cd92e1b27a37b51b16047350457114bc45
           Ir a Gestión de Proyectos
         </button>
       </div>
